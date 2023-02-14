e446409729aa7a70511590be0a0b9e6d31c0774d
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-14 20:26:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,61 @@
-import React, { useEffect, useState } from "react";
-import "../styles/App.css";
+import React, { useState } from "react";
+
+import "./App.css";
 
 const App = () => {
-  //code here
-  const [className, setClassName] = useState("redColor");
-  const [value, setValue] = useState(true);
 
-  function onButtonClick() {
-    setValue(!value);
-    // true to false and false to true
-  }
+//code here
 
-  // whenever react component loads for the first time
-  // or whenever we want to monitor any change in a state
-  useEffect(() => {
-    if (value === true) {
-      setClassName("redColor");
-    }
-    if (value === false) {
-      setClassName("blueColor");
-    }
-  }, [value]); // dependency array - whatever variable is inside this,
-  // whenever it changes, run useEffect.
+// const [className, setClassName] = useState("redColor");
 
-  return (
-    <div id="main">
-      <p className={className}>Newton School</p>
-      <button id="button" onClick={onButtonClick}>
-        Change Style
-      </button>
-    </div>
-  );
+const [value, setValue] = useState(true);
+
+function onButtonClick() {
+
+setValue(!value);
+
+// true to false and false to true
+
+}
+
+// whenever react component loads for the first time
+
+// or whenever we want to monitor any change in a state
+
+// useEffect(() => {
+
+// if (value === true) {
+
+// setClassName("redColor");
+
+// }
+
+// if (value === false) {
+
+// setClassName("blueColor");
+
+// }
+
+// }, [value]); // dependency array - whatever variable is inside this,
+
+// whenever it changes, run useEffect.
+
+return (
+
+<div id="main">
+
+<p className={value?'redColor':'blueColor'}>Newton School</p>
+
+<button id="button" onClick={onButtonClick}>
+
+Change Style
+
+</button>
+
+</div>
+
+);
+
 };
 
 export default App;
